229d85f1cc548a86da2abbda3dbd4434679a9c93
marcorogo <marcoprimiceri82@gmail.com>
2016-01-09 21:48:15 +0100

    Controller Helicopter + Prefabs

--- ProjectPhoenix/Assets/Src/Controllers/Player/ControllerPlayerHeli.cs
+++ ProjectPhoenix/Assets/Src/Controllers/Player/ControllerPlayerHeli.cs
@@ -10,109 +10,157 @@ internal class ControllerPlayerHeli : MonoBehaviour, IControllerPlayer
     public float VelocityForce;
     public float VelocityRotation;
 
+    Plane playerPlane;
     Rigidbody heliRigidbody;
-    bool isFlying;
+    Vector3 mousePos;
+    bool isGrounded;
+    float mass;
+    float engineForce;
+    float forwardForce;
+    float strafeForce;
+    float currentForwardSlope;
+    float currentStrafeSlope;
     void Awake()
     {
         this.heliRigidbody = GetComponent<Rigidbody>();
-    }
+        this.mass = heliRigidbody.mass;
+        this.isGrounded = true;
+        this.playerPlane = new Plane(Vector3.up, this.transform.position);
 
+    }
+    void FixedUpdate()
+    {
+        LiftProcess();
+        if (!isGrounded)
+        {
+            Move();
+        }
+    }
+    void Update()
+    {
+        if (!isGrounded)
+        {
+            Inclination();
+            Rotation();
+        }
+    }
+    private void LiftProcess()
+    {
+        var upForce = 1f - Mathf.Clamp01(this.transform.position.y / MaxHeight);
+        upForce = Mathf.Lerp(0f, engineForce, upForce) * mass;
+        this.heliRigidbody.AddForce(Vector3.up * upForce);
+    }
+    private void Move()
+    {
+        this.heliRigidbody.AddForce(heliRigidbody.transform.forward * VelocityForce * forwardForce);
+        this.heliRigidbody.AddForce(heliRigidbody.transform.right * VelocityForce * strafeForce);
+        this.heliRigidbody.velocity = Vector3.ClampMagnitude(this.heliRigidbody.velocity, MaxVelocityMagnitude);
+    }
+    private void Inclination()
+    {
+        this.currentForwardSlope = Mathf.Lerp(currentForwardSlope, forwardForce * 20f, Time.fixedDeltaTime);
+        this.currentStrafeSlope = Mathf.Lerp(currentStrafeSlope, strafeForce * 20f, Time.fixedDeltaTime);
+        this.OwnBody.transform.localRotation = Quaternion.Euler(currentForwardSlope, OwnBody.transform.localEulerAngles.y, -currentStrafeSlope);
+    }
+    private void Rotation()
+    {
+        playerPlane.SetNormalAndPosition(Vector3.up, this.transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        float hitdist = 0.0f;
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - this.transform.position);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, VelocityRotation * Time.deltaTime);
+        }
+    }
     public void BeginBackward()
     {
-        throw new NotImplementedException();
+        this.forwardForce = -1f;
     }
 
     public void BeginDown()
     {
-        throw new NotImplementedException();
+        this.isGrounded = true;
+        this.engineForce = 0f;
     }
 
     public void BeginFire()
     {
-        throw new NotImplementedException();
     }
 
     public void BeginForward()
     {
-        throw new NotImplementedException();
+        this.forwardForce = 1f;
+
     }
 
     public void BeginPanLeft()
     {
-        throw new NotImplementedException();
+        this.strafeForce = -1f;
     }
 
     public void BeginPanRight()
     {
-        throw new NotImplementedException();
+        this.strafeForce = 1f;
     }
 
     public void BeginTurnLeft()
     {
-        throw new NotImplementedException();
     }
 
     public void BeginTurnRight()
     {
-        throw new NotImplementedException();
     }
 
     public void BeginUp()
     {
-        if(!isFlying)
-        {
-            this.heliRigidbody.AddForce(Vector3.up * 70f);
-            this.isFlying = true;
-        }
+        this.isGrounded = false;
+        this.engineForce = 50f;
     }
 
     public void EndBackward()
     {
-        throw new NotImplementedException();
+        this.forwardForce = 0f;
     }
 
     public void EndDown()
     {
-        throw new NotImplementedException();
     }
 
     public void EndFire()
     {
-        throw new NotImplementedException();
     }
 
     public void EndForward()
     {
-        throw new NotImplementedException();
+        this.forwardForce = 0f;
     }
 
     public void EndPanLeft()
     {
-        throw new NotImplementedException();
+        this.strafeForce = 0f;
     }
 
     public void EndPanRight()
     {
-        throw new NotImplementedException();
+        this.strafeForce = 0f;
     }
 
     public void EndTurnLeft()
     {
-        throw new NotImplementedException();
     }
 
     public void EndTurnRight()
     {
-        throw new NotImplementedException();
     }
 
     public void EndUp()
     {
-        throw new NotImplementedException();
     }
 
     public void MousePosition(Vector3 vMousePosition)
     {
-        throw new NotImplementedException();
+        this.mousePos = vMousePosition;
     }
 }
