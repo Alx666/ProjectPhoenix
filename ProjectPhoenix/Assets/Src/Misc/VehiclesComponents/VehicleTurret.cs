410ca667de5ffa740b826e09299536b970b6dc0b
Frippo <francescofrippo@gmail.com>
2016-02-10 11:01:48 +0100

    Vehcile Turret Work in Progress 2

--- ProjectPhoenix/Assets/Src/Misc/VehiclesComponents/VehicleTurret.cs
+++ ProjectPhoenix/Assets/Src/Misc/VehiclesComponents/VehicleTurret.cs
@@ -10,21 +10,23 @@ internal class VehicleTurret : MonoBehaviour
     [SerializeField]
     internal float RotationSpeed;
 
-    public Vector3 debugRotation;
-    public Vector3 debugScreenDistance;
-    public Vector3 debugNewRot;
+    private Plane playerPlane;
+
+    void Awake()
+    {
+        this.playerPlane = new Plane(Vector3.up, this.transform.position);
+    }
 
     internal void UpdateRotation(Vector3 MousePosition)
     {
-        Vector3 screenDistance = (MousePosition - (Camera.main.WorldToScreenPoint(this.transform.position))).normalized;
-        //Vector3 newRotation = (Quaternion.RotateTowards(AxeY.transform.rotation, Quaternion.LookRotation(direction), RotationSpeed)).eulerAngles;
-        //AxeY.transform.rotation = Quaternion.Euler(AxeY.transform.rotation.x, newRotation.y, AxeY.transform.rotation.z);
-
-        Vector3 newRot = (Quaternion.RotateTowards(AxeY.transform.rotation, Quaternion.LookRotation(screenDistance), RotationSpeed)).eulerAngles;
-        AxeY.transform.rotation = Quaternion.Euler(AxeY.transform.rotation.x, newRot.y, AxeY.transform.rotation.z);
-
-        debugRotation = AxeY.transform.rotation.eulerAngles;
-        debugScreenDistance = screenDistance;
-        debugNewRot = newRot;
+        playerPlane.SetNormalAndPosition(Vector3.up, this.transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(MousePosition);
+        float hitdist = 0.0f;
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - AxeY.transform.position);
+            AxeY.transform.rotation = Quaternion.Slerp(AxeY.transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+        }
     }
 }
