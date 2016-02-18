e95023917594e27dc68b34a11e596e1bb8df06e6
SaverioDiLazzaro <saverio.dilazzaro@gmail.com>
2016-02-18 12:39:28 +0100

    updated Vehicle Tester
    Ora basta premere i tasti: 1-2-3-4 per cambiare veicolo in scena (non
    supporta più di 4 veicoli in scena)

--- ProjectPhoenix/Assets/Src/Misc/Diagnostic/VehicleTester.cs
+++ ProjectPhoenix/Assets/Src/Misc/Diagnostic/VehicleTester.cs
@@ -1,36 +1,88 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 internal class VehicleTester : MonoBehaviour
 {
-    [SerializeField]
-    internal List<GameObject> InputReceivers;
+    private List<GameObject> Targets;
+    private GameObject currentTarget;
 
-	void Awake ()
+    public GameObject Camera;
+    public float CameraSpeed { get; private set; }
+    public float CameraTolerance { get; private set; }
+
+    public void Awake()
     {
-        InputReceivers.ForEach(hGO => hGO.GetComponent<InputProviderPCStd>().enabled = false);
-        InputReceivers.First().GetComponent<InputProviderPCStd>().enabled = true;
+        Targets = new List<GameObject>();
+
+        CameraSpeed = 10f;
+        CameraTolerance = 1f;
     }
-	
-	void Update ()
+
+    public void Start()
     {
-        if (Input.GetMouseButtonDown(0))
+        Targets = FindObjectsOfType<GameObject>().Where(GO => GO.GetComponent<IControllerPlayer>() != null).ToList();
+
+        if (Targets[0] != null)
         {
-            RaycastHit hit;
+            Targets.ForEach(GO => GO.GetComponent<InputProviderPCStd>().enabled = false);
+            Targets[0].GetComponent<InputProviderPCStd>().enabled = true;
+            currentTarget = Targets[0];
+        }
+    }
 
-            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+    public void Update()
+    {
+        InputUpdate();
+        UpdateCameraPosition();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        Vector3 target = currentTarget.transform.position;
+        Camera.transform.position = new Vector3(target.x, Camera.transform.position.y, target.z);
+    }
+
+    private void InputUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (Targets[0] != null)
             {
-                InputProviderPCStd provider = hit.collider.transform.root.GetComponent<InputProviderPCStd>();
-                if (provider && InputReceivers.Contains(provider.gameObject))
-                {
-                    Debug.Log(hit.transform.root.gameObject.name + " Selected!");
-
-                    InputReceivers.Where(hGO => hGO != provider.gameObject).ToList().ForEach(hGO => hGO.GetComponent<InputProviderPCStd>().enabled = false);
-                    provider.enabled = true;
-                }
+                Targets.ForEach(GO => GO.GetComponent<InputProviderPCStd>().enabled = false);
+                Targets[0].GetComponent<InputProviderPCStd>().enabled = true;
+                currentTarget = Targets[0];
             }
         }
-	}
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (Targets[1] != null)
+            {
+                Targets.ForEach(GO => GO.GetComponent<InputProviderPCStd>().enabled = false);
+                Targets[1].GetComponent<InputProviderPCStd>().enabled = true;
+                currentTarget = Targets[1];
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (Targets[2] != null)
+            {
+                Targets.ForEach(GO => GO.GetComponent<InputProviderPCStd>().enabled = false);
+                Targets[2].GetComponent<InputProviderPCStd>().enabled = true;
+                currentTarget = Targets[2];
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            if (Targets[3] != null)
+            {
+                Targets.ForEach(GO => GO.GetComponent<InputProviderPCStd>().enabled = false);
+                Targets[3].GetComponent<InputProviderPCStd>().enabled = true;
+                currentTarget = Targets[3];
+            }
+        }
+    }
 }
