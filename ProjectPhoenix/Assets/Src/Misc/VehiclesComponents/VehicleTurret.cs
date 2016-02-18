04364ab58f08b2f746e52f936ea066561b448074
Frippo <francescofrippo@gmail.com>
2016-02-18 15:37:36 +0100

    Turret fix

--- ProjectPhoenix/Assets/Src/Misc/VehiclesComponents/VehicleTurret.cs
+++ ProjectPhoenix/Assets/Src/Misc/VehiclesComponents/VehicleTurret.cs
@@ -10,6 +10,8 @@ internal class VehicleTurret : MonoBehaviour
     [SerializeField]
     internal float RotationSpeed;
 
+    public GameObject BulletLocator;
+
     private Plane playerPlane;
 
     void Awake()
@@ -19,25 +21,40 @@ internal class VehicleTurret : MonoBehaviour
 
     internal void UpdateRotation(Vector3 MousePosition)
     {
-        //Ray ray = Camera.main.ScreenPointToRay(MousePosition);
-        //RaycastHit vHit;
-        //if (Physics.Raycast(ray.origin, ray.direction, out vHit, Mathf.Infinity))
-        //{
-        //    Quaternion targetRotation = Quaternion.LookRotation(vHit.point - AxeY.transform.position);
-        //    AxeY.transform.rotation = Quaternion.Slerp(AxeY.transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
-        //}
-        //temporaneo
         if (AxeY != null)
         {
-            playerPlane.SetNormalAndPosition(Vector3.up, this.transform.position);
             Ray ray = Camera.main.ScreenPointToRay(MousePosition);
-            float hitdist = 0.0f;
-            if (playerPlane.Raycast(ray, out hitdist))
+            RaycastHit vHit;
+            if (Physics.Raycast(ray.origin, ray.direction, out vHit, Mathf.Infinity))
             {
-                Vector3 targetPoint = ray.GetPoint(hitdist);
-                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - AxeY.transform.position);
-                AxeY.transform.rotation = Quaternion.Slerp(AxeY.transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+                Vector3 vDirection = vHit.point;// - AxeY.transform.position;
+                Quaternion targetRotation = Quaternion.LookRotation(vDirection);
+                AxeY.transform.localRotation = Quaternion.RotateTowards(AxeY.transform.localRotation, targetRotation, RotationSpeed);
+                AxeY.transform.localRotation = Quaternion.Euler(0f, AxeY.transform.localRotation.eulerAngles.y, 0f);
+
+                Debug.Log(vDirection); //altezza massima obj
+                // BulletLocator.transform.localRotation = Quaternion.RotateTowards(AxeX.transform.localRotation, targetRotation, RotationSpeed);
+                // BulletLocator.transform.localRotation = Quaternion.Euler(BulletLocator.transform.localRotation.x, 0f,0f);
+                Quaternion provvisorio = BulletLocator.transform.localRotation;
+                provvisorio = Quaternion.Slerp(BulletLocator.transform.localRotation, targetRotation, RotationSpeed);
+                provvisorio.y = 0;
+                provvisorio.z = 0;
+                BulletLocator.transform.localRotation = provvisorio;
             }
         }
+
+        //temporaneo
+        //if (AxeY != null)
+        //{
+        //    //playerPlane.SetNormalAndPosition(Vector3.up, this.transform.position);
+        //    //Ray ray = Camera.main.ScreenPointToRay(MousePosition);
+        //    //float hitdist = 0.0f;
+        //    //if (playerPlane.Raycast(ray, out hitdist))
+        //    //{
+        //    //    Vector3 targetPoint = ray.GetPoint(hitdist);
+        //    //    Quaternion targetRotation = Quaternion.LookRotation(targetPoint - AxeY.transform.position);
+        //    //    AxeY.transform.rotation = Quaternion.Slerp(AxeY.transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+        //    //}
+        //}
     }
 }
