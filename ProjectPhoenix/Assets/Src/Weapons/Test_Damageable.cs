c252ebb2998dd505805b050e0018fc44272608cd
MarcoMoscati88 <marco.moscati88@gmail.com>
2016-03-21 16:33:55 +0100

    Damageable_Test
    Se scoppia la colpa è dell'Isis

---
+++ ProjectPhoenix/Assets/Src/Weapons/Test_Damageable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+
+public class Test_Damageable:MonoBehaviour, IDamageable
+{
+	public float fHp;
+
+	public Test_Damageable()
+	{
+		
+	}
+
+	public void Damage( float fDmg )
+	{
+		this.fHp -= fDmg;
+		if ( this.fHp <= 0f )
+		{
+			GameObject.Destroy( this.gameObject );
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
