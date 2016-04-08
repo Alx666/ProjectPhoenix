db243846aa7c48685ebdf0c67234631b380724d2
adrN90 <adrian.nom@hotmail.it>
2016-04-08 14:07:14 +0200

    CustomCamera v1.0

--- ProjectPhoenix/Assets/Src/Utils/CustomCamera.cs
+++ ProjectPhoenix/Assets/Src/Utils/CustomCamera.cs
@@ -11,6 +11,7 @@ public class CustomCamera : MonoBehaviour
     [Range(0, 1)]
     public float AimOffset;
 
+
     ICameraState current;
     StandardCamera stdCamera;
     AimCamera aimCamera;
@@ -43,7 +44,6 @@ public class CustomCamera : MonoBehaviour
         current.CalculateOffset();
 
         this.transform.position = Target.GetComponent<Rigidbody>().transform.position + Offset;
-        Debug.DrawLine(this.transform.position, this.transform.forward * 20f, Color.red);
     }
 
     Vector3 InitialOffset()
@@ -104,49 +104,15 @@ public class CustomCamera : MonoBehaviour
                     cameraIsSaved = true;
                 }
 
-
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit vHit;
 
                 if (Physics.Raycast(ray, out vHit))
                 {
-                    //camera.Offset = startingOffset;
-                    //distanceFromTarget = new Vector3(vHit.point.x, camera.Target.transform.position.y, vHit.point.z);//new Ray(camera.Target.transform.position, camera.AimOffset * (vHit.point - camera.Target.transform.position));
-                    //TargetAim.x = ((vHit.point.x - camera.Offset.x) * camera.AimOffset) - savedOffset.x;
-                    //TargetAim.z = ((vHit.point.z - camera.Offset.z) * camera.AimOffset) - savedOffset.z;
-                    camera.Offset.x = savedCamera.Offset.x + ((vHit.point.x - (camera.Target.transform.position.x - savedCamera.Target.transform.position.x)) * camera.AimOffset) * Time.deltaTime;
-                    camera.Offset.z = savedCamera.Offset.z + ((vHit.point.z - (camera.Target.transform.position.z - savedCamera.Target.transform.position.z)) * camera.AimOffset) * Time.deltaTime;
-                    camera.Offset = savedCamera.Offset;
-
-                    cameraIsSaved = false;
-                    //Debug.DrawRay(camera.Target.transform.position * camera.AimOffset, distanceFromTarget, Color.red);
-                    //Debug.DrawRay(ray.origin, ray.direction * vHit.distance, Color.blue);
-                    //
+                        TargetAim = camera.Offset + (camera.AimOffset * (vHit.point - camera.Target.transform.position));
+                        camera.Offset = new Vector3(Mathf.Lerp(camera.Offset.x, TargetAim.x, Time.deltaTime), camera.Offset.y, Mathf.Lerp(camera.Offset.z, TargetAim.z, Time.deltaTime));
                 }
-
-
-
-
-                //    //distanceFromTarget = ray.point - target.position;
-                //    //Offset = distanceFromTarget.normalized * Mathf.LerpAngle(0f, distanceFromTarget.magnitude, Time.deltaTime);
-                //    //Offset.y = savedOffset.y;
-                //    //-------------------------------------------------------------
-                //    //mousePos.z = 10F;
-                //    //Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-                //    //player2.transform.position = worldPos;
-
-
-                //    //Vector3 mid = player2.transform.position - player1.transform.position;
-                //    //middlePoint = player1.transform.position + 0.5f * mid;
-
-                //    //this.transform.position = new Vector3(middlePoint.x, 0, -10);
             }
-            //if (Input.GetKeyUp(camera.StateChanger))
-            //{
-            //    camera.Offset = savedOffset;
-            //    offsetIsSaved = false;
-            //}
         }
     }
 }
