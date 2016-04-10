7756fc8736e352f91922a6b1367f7679816e40cc
Francesco Ippolito <francescofrippo@gmail.com>
2016-04-10 18:03:58 +0200

    Adding audio to muzzle and PartConfig prefabs

--- ProjectPhoenix/Assets/Src/Particles/SimpleVfx.cs
+++ ProjectPhoenix/Assets/Src/Particles/SimpleVfx.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 
 public class SimpleVfx : MonoBehaviour, IVisualEffect, IPoolable
 {
-    public float Duration = 0.02f;
+    public float MuzzleDuration = 0.02f;
+    public float SoundDuration = 1f;
 
     public Pool Pool { get; set; }
 
     private AudioSource m_hAudioSource;
+    private Light m_hPointLight;
+    private List<MeshRenderer> m_hChildrenQuad;
 
     void Awake()
     {
         m_hAudioSource = this.GetComponent<AudioSource>();
+        m_hPointLight = this.GetComponentInChildren<Light>();
+        m_hChildrenQuad = new List<MeshRenderer>(GetComponentsInChildren<MeshRenderer>());
     }
 
     public void PlayEffect(Vector3 vPosition, Vector3 vDirection, float scaleCoef, bool isSide)
@@ -28,10 +34,16 @@ public class SimpleVfx : MonoBehaviour, IVisualEffect, IPoolable
 
         m_hAudioSource.Play();
 
-        StartCoroutine(Wait(Duration));
+        StartCoroutine(WaitForMuzzle(MuzzleDuration));
+        StartCoroutine(WaitForSound(SoundDuration));
     }
 
-    IEnumerator Wait(float duration)
+    IEnumerator WaitForMuzzle(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        DisableMuzzle();
+    }
+    IEnumerator WaitForSound(float duration)
     {
         yield return new WaitForSeconds(duration);
         GlobalFactory.Recycle(this.gameObject);
@@ -39,6 +51,8 @@ public class SimpleVfx : MonoBehaviour, IVisualEffect, IPoolable
 
     public void Enable()
     {
+        m_hPointLight.enabled = true;
+        m_hChildrenQuad.ForEach(hQ => hQ.enabled = true);
         m_hAudioSource.time = 0f;
         this.gameObject.SetActive(true);
     }
@@ -47,5 +61,11 @@ public class SimpleVfx : MonoBehaviour, IVisualEffect, IPoolable
     {
         m_hAudioSource.Stop();
         this.gameObject.SetActive(false);
+    }
+
+    private void DisableMuzzle()
+    {
+        m_hPointLight.enabled = false;
+        m_hChildrenQuad.ForEach(hQ => hQ.enabled = false);
     }
 }
