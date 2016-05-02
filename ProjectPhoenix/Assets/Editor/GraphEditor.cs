8146a954be8b9e2afebf7188d0e4a51e0be5e682
MarcoMoscati88 <marco.moscati88@gmail.com>
2016-05-03 01:09:07 +0200

    Editor rifatto
    Ora graficamente non dovrebbe più resizare niente.

--- ProjectPhoenix/Assets/Editor/GraphEditor.cs
+++ ProjectPhoenix/Assets/Editor/GraphEditor.cs
@@ -14,6 +14,7 @@ public class GraphEditor : Editor
     private string Label0 = string.Empty;
     private string Label1 = string.Empty;
 	private string Label2 = string.Empty;
+	private GUIStyle _guiStyle = new GUIStyle( GUI.skin.label ) { fixedWidth = 250 };
 
     void OnEnable()
     {
@@ -42,50 +43,16 @@ public class GraphEditor : Editor
         a.Successors.Add(b);
     }
 
-    //This is method is called by the unity editor when it need to draw the inspector
-    public override void OnInspectorGUI()
-    {
-        //The inherited OnInspectorGUI draws inspector representations of public variables.
-        //we don't need it
-        //base.OnInspectorGUI(); 
-
-        EditorGUILayout.Separator();
-        EditorGUILayout.BeginVertical(EditorStyles.objectFieldThumb);
-        EditorGUILayout.Separator();
-
-
-		for ( int i = 0; i < m_hTarget.Nodes.Count; i++)
-        {
-			Node hNode = m_hTarget.Nodes[i];
-			EditorGUILayout.BeginHorizontal();
-			EditorGUILayout.LabelField( "N° ", hNode.GetID() );
-			hNode.nodetype = (NodeType)EditorGUILayout.EnumPopup( hNode.nodetype );
-			EditorGUILayout.EndHorizontal();
-            hNode.Position = EditorGUILayout.Vector3Field("", hNode.Position);
-        }
-
-        EditorGUILayout.Separator();
+	//This is method is called by the unity editor when it need to draw the inspector
+	public override void OnInspectorGUI()
+	{
+		EditorGUILayout.Separator();
+		EditorGUILayout.BeginVertical( EditorStyles.objectFieldThumb, GUILayout.MinWidth(252), GUILayout.MaxWidth(252));
 		EditorGUILayout.BeginHorizontal();
 
-        if (GUILayout.Button("Add"))
-        {
-            Node hNew = ScriptableObject.CreateInstance(typeof(Node)) as Node;
-
-            RaycastHit vHit;
-            Ray vRay = new Ray(SceneView.lastActiveSceneView.camera.transform.position, SceneView.lastActiveSceneView.camera.transform.forward);
-            if (Physics.Raycast(vRay, out vHit))
-            {
-                hNew.Position = vHit.point;
-            }
-
-            hNew.Label = hNew.SetNodeId();
-            m_hTarget.Nodes.Add(hNew);
-        }
-
-		if ( GUILayout.Button( "Add & Connect" ) )
+		if ( GUILayout.Button( "Add", GUILayout.Width( 122 ) ) )
 		{
 			Node hNew = ScriptableObject.CreateInstance( typeof( Node ) ) as Node;
-
 			RaycastHit vHit;
 			Ray vRay = new Ray( SceneView.lastActiveSceneView.camera.transform.position, SceneView.lastActiveSceneView.camera.transform.forward );
 			if ( Physics.Raycast( vRay, out vHit ) )
@@ -95,8 +62,22 @@ public class GraphEditor : Editor
 
 			hNew.Label = hNew.SetNodeId();
 			m_hTarget.Nodes.Add( hNew );
+		}
 
-			if ( m_hTarget.Nodes.Count>1 )
+		if ( GUILayout.Button( "Add & Connect", GUILayout.Width( 122 ) ) )
+		{
+			Node hNew = ScriptableObject.CreateInstance( typeof( Node ) ) as Node;
+			RaycastHit vHit;
+			Ray vRay = new Ray( SceneView.lastActiveSceneView.camera.transform.position, SceneView.lastActiveSceneView.camera.transform.forward );
+
+			if ( Physics.Raycast( vRay, out vHit ) )
+			{
+				hNew.Position = vHit.point;
+			}
+			hNew.Label = hNew.SetNodeId();
+			m_hTarget.Nodes.Add( hNew );
+
+			if ( m_hTarget.Nodes.Count > 1 )
 			{
 				Node a = m_hTarget.Nodes[m_hTarget.Nodes.Count - 2];
 				Node b = m_hTarget.Nodes.Last();
@@ -105,105 +86,116 @@ public class GraphEditor : Editor
 			}
 		}
 
-
-        if (GUILayout.Button("Connect"))
-        {
-            IEnumerable<Node> hNodes = m_hTarget.Nodes.Where(x => x.Label == Label0 || x.Label == Label1);
-
-            if (hNodes.Count() == 2)
-            {
-                Node a = hNodes.First();
-                Node b = hNodes.Last();
-                a.Successors.Add(b);
-                b.Successors.Add(a);
-            }
-            Label0 = string.Empty;
-            Label1 = string.Empty;
-        }
-
-        Label0 = GUILayout.TextField(Label0);
-        Label1 = GUILayout.TextField(Label1);
-
-        EditorGUILayout.EndHorizontal();
-		EditorGUILayout.Separator();
+		EditorGUILayout.EndHorizontal();
 		EditorGUILayout.BeginHorizontal();
 
-		if ( GUILayout.Button("Remove" ))
+		if ( GUILayout.Button( "Connect", GUILayout.Width( 122 ) ) )
+		{
+			IEnumerable<Node> hNodes = m_hTarget.Nodes.Where( x => x.Label == Label0 || x.Label == Label1 );
+
+			if ( hNodes.Count() == 2 )
+			{
+				Node a = hNodes.First();
+				Node b = hNodes.Last();
+				a.Successors.Add( b );
+				b.Successors.Add( a );
+			}
+			Label0 = string.Empty;
+			Label1 = string.Empty;
+		}
+
+		Label0 = GUILayout.TextField( Label0, 6, GUILayout.Width( 59 ) );
+		Label1 = GUILayout.TextField( Label1, 6, GUILayout.Width( 59 ) );
+
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.BeginHorizontal();
+
+		if ( GUILayout.Button( "Remove", GUILayout.Width( 122 ) ) )
 		{
 			Node hNode = m_hTarget.Nodes.Where( x => x.Label == Label2 ).First();
 			hNode.Successors.ForEach( hS => hS.Successors.Remove( hNode ) );
 			m_hTarget.Nodes.Remove( hNode );
 			Label2 = string.Empty;
 		}
-		Label2 = GUILayout.TextField( Label2 );
 
-		EditorGUILayout.Separator();
+		Label2 = GUILayout.TextField( Label2, 6, GUILayout.Width( 59 ) );
 
-		if ( GUILayout.Button("Reset Graph" ))
+		if ( GUILayout.Button( "Load", GUILayout.Width( 59 ) ) ) 
+		{
+			Graph<POI> m_hGraph = GraphParser.Instance.Parse( "Graph.txt" );
+			m_hGraph.m_hNodes.ForEach( hN =>
+			 {
+				 this.AddNode( hN );
+			 } );
+			m_hGraph.m_hNodes.ForEach( hN =>
+			{
+				hN.neighbours.ForEach( hNeigh =>
+				 {
+				 this.AddLink( hN, hNeigh.Node );
+				 });
+			});
+        }
+
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.BeginHorizontal();
+
+		if ( GUILayout.Button("Build", GUILayout.Width(122)) )
+		{
+			Graph<POI> graph = new Graph<POI>();
+			m_hTarget.Nodes.ForEach( hN =>
+			{
+				POI p = new POI( hN.Position, hN.nodetype );
+				graph.Add( new Node<POI>( p, int.Parse( hN.GetID() ) ) );
+			});
+
+			m_hTarget.Nodes.ForEach( hN =>
+			{
+				hN.Successors.ForEach( hS =>
+				{
+					graph.m_hNodes[int.Parse( hN.GetID() )].Link( graph.m_hNodes[int.Parse( hS.GetID() )], Vector3.Distance( hS.Position, hN.Position ) );
+				});
+			});
+
+			using ( StreamWriter hS = new StreamWriter( "Graph.txt" ) )
+			{
+				graph.m_hNodes.ForEach( hN =>
+				{
+					hS.WriteLine( "n/" + hN.value.Position.x + ", " + hN.value.Position.y + ", " + hN.value.Position.z + "/" + hN.value.Type + "/" + hN.m_iId );
+				});
+
+				graph.m_hNodes.ForEach( hN =>
+				{
+					hN.neighbours.ForEach( hNeigh =>
+					{
+						hS.Write( "l/" + hN.m_iId );
+						hS.WriteLine( "/" + hNeigh.Node.m_iId + "/" + hNeigh.Distance );
+					} );
+				});
+			}
+		}
+
+		if ( GUILayout.Button("Reset Graph", GUILayout.Width(122)) )
 		{
 			m_hTarget.ResetGraph();
 			Node.ResetNodeId();
 		}
+
 		EditorGUILayout.EndHorizontal();
+
+		for ( int i = 0; i < m_hTarget.Nodes.Count; i++ )
+		{
+			Node hNode = m_hTarget.Nodes[i];
+			EditorGUILayout.BeginHorizontal(GUILayout.Width(250));
+			EditorGUILayout.LabelField( "N° " + hNode.GetID(), GUILayout.MinWidth(122), GUILayout.MaxWidth(122));
+			hNode.nodetype = (NodeType)EditorGUILayout.EnumPopup( hNode.nodetype, GUILayout.MinWidth(122), GUILayout.MaxWidth(122));
+			EditorGUILayout.EndHorizontal();
+			hNode.Position = EditorGUILayout.Vector3Field( "", hNode.Position, GUILayout.Width(250));
+		}
+
 		EditorGUILayout.EndVertical();
 
-        if (GUILayout.Button("Build"))
-        {
-            Graph<POI> graph = new Graph<POI>();
-            m_hTarget.Nodes.ForEach(hN =>
-            {
-                POI p = new POI(hN.Position, hN.nodetype);
-                graph.Add(new Node<POI>(p, int.Parse(hN.GetID())));
-            });
-
-            m_hTarget.Nodes.ForEach(hN =>
-            {
-                hN.Successors.ForEach(hS =>
-                {
-                    graph.m_hNodes[int.Parse(hN.GetID())].Link(graph.m_hNodes[int.Parse(hS.GetID())], Vector3.Distance(hS.Position, hN.Position));
-                });
-            });
-
-            using (StreamWriter hS = new StreamWriter("Graph.txt"))
-            {
-                graph.m_hNodes.ForEach(hN =>
-                {
-                    hS.WriteLine("n/" + hN.value.Position.x + ", " + hN.value.Position.y + ", " + hN.value.Position.z + "/" + hN.value.Type + "/" + hN.m_iId);
-                });
-
-                graph.m_hNodes.ForEach(hN =>
-                {
-                    hN.neighbours.ForEach(hNeigh =>
-                    {
-                        hS.Write("l/" + hN.m_iId);
-                        hS.WriteLine("/" + hNeigh.Node.m_iId + "/" + hNeigh.Distance);
-                    });
-                });
-            }
-        }
-
-        if (GUILayout.Button("Load"))
-        {
-            Graph<POI> m_hGraph = GraphParser.Instance.Parse("Graph.txt");
-
-            m_hGraph.m_hNodes.ForEach(hN =>
-            {
-                this.AddNode(hN);
-            });
-
-            m_hGraph.m_hNodes.ForEach(hN =>
-            {
-                hN.neighbours.ForEach(hNeigh =>
-                {
-                    this.AddLink(hN, hNeigh.Node);
-                });
-            });
-        }
-
-        SceneView.RepaintAll();
-    }
-
-
+		SceneView.RepaintAll();
+	}
     void OnSceneGUI()
     {
         for (int i = 0; i < m_hTarget.Nodes.Count; i++)
