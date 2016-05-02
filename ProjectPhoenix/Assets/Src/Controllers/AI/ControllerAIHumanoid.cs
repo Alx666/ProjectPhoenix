a42a3b8f7c88a4d52bb89500427772b11c819d66
marcorogo <marcoprimiceri82@gmail.com>
2016-05-02 15:36:23 +0200

    Revert "Vanguard AI"
    This reverts commit 307022118c2eff63ab08adcc247a9301113317f0.

--- ProjectPhoenix/Assets/Src/Controllers/AI/ControllerAIHumanoid.cs
+++ ProjectPhoenix/Assets/Src/Controllers/AI/ControllerAIHumanoid.cs
@@ -4,77 +4,43 @@ using System;
 
 public class ControllerAIHumanoid : MonoBehaviour
 {
-    public Transform RightHand;
-    public Transform WeaponLocator;
-    public Transform LeftHandObj = null;
-    public Transform LookObj = null;
-
-    public Transform CurrentEnemy { get; private set; }
-    float fieldOfView = 110f;
     Vector3 m_GroundNormal;
     float turnValue;
     float forwardValue;
-    int forwardHash;
-    int turnHash;
-    int toChase;
+    Vector3 target;
+    //int run;
     Animator animator;
     NavMeshAgent agent;
-    SphereCollider coll;
-    void OnTriggerStay(Collider other)
-    {
-        if(other.gameObject.tag == "Enemy")
-        {
-            Vector3 direction = other.transform.position - this.transform.position;
-            float angle = Vector3.Angle(this.transform.forward, direction);
-            if(angle <= fieldOfView * 0.5f)
-            {
-                this.CurrentEnemy = other.transform;
-                animator.SetTrigger(toChase);
-            }
-        }
-    }
-    void OnAnimatorIK()
-    {
-        
-        if (animator)
-        {
-            if (LookObj != null)
-            {
-                animator.SetLookAtWeight(1);
-                animator.SetLookAtPosition(LookObj.position);
-            }
-
-            if (LeftHandObj != null)
-            {
-                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
-                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
-                animator.SetIKPosition(AvatarIKGoal.LeftHand, LeftHandObj.position);
-                animator.SetIKRotation(AvatarIKGoal.LeftHand, LeftHandObj.rotation);
-            }
-            else
-            {
-                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
-                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
-                animator.SetLookAtWeight(0);
-            }
-            
-        }
-    }
     void Awake()
     {
         this.agent = GetComponent<NavMeshAgent>();
         this.animator = GetComponent<Animator>();
-        this.coll = GetComponent<SphereCollider>();
-        this.forwardHash = Animator.StringToHash("Forward");
-        this.turnHash = Animator.StringToHash("Turn");
-        this.toChase = Animator.StringToHash("ToChase");
-
+        //this.run = Animator.StringToHash("Run");
     }
+    // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
 
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                target = hit.point;
+            }
+
+        }
+        if (target != null)
+            agent.SetDestination(target);
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+            Move(agent.desiredVelocity);
+        else
+        {
+            Move(Vector3.zero);
+        }
     }
-    internal void Move(Vector3 move)
+    void Move(Vector3 move)
     {
         if (move.magnitude > 1f)
             move.Normalize();
@@ -97,28 +63,13 @@ public class ControllerAIHumanoid : MonoBehaviour
         float turnSpeed = Mathf.Lerp(180f, 360f, forwardValue);
         transform.Rotate(0f, turnValue * turnSpeed * Time.deltaTime, 0f);
     }
-    
     void SetAnimator(Vector3 move)
     {
-        animator.SetFloat(forwardHash, forwardValue, 0.1f, Time.deltaTime);
-        animator.SetFloat(turnHash, turnValue, 0.1f, Time.deltaTime);
+        animator.SetFloat("Forward", forwardValue, 0.1f, Time.deltaTime);
+        animator.SetFloat("Turn", turnValue, 0.1f, Time.deltaTime);
         if (move.magnitude > 0)
         {
             animator.speed = 1;
         }
     }
-    internal void ManualSetAnimator(float forward, float turn, bool damp)
-    {
-        if (damp)
-        {
-            animator.SetFloat(forwardHash, forward, 0.1f, Time.deltaTime);
-            animator.SetFloat(turnHash, turn, 0.1f, Time.deltaTime);
-        }
-        else
-        {
-            animator.SetFloat(forwardHash, forward);
-            animator.SetFloat(turnHash, turn);
-        }
-    }
-   
 }
