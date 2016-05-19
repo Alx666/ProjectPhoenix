e36b983ff762fb19dd5fe3647e5e7170ee4cb008
xantes90 <samuelef@alice.it>
2016-05-19 14:52:17 +0200

    Actor

---
+++ ProjectPhoenix/Assets/Src/Actor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
+
+public class Actor : NetworkBehaviour, IDamageable
+{
+    [SyncVar]
+    [SerializeField]
+    private float Hp;
+    [SerializeField]
+    private ArmorType Armor;
+    //[SerializeField]private List<Skill> m_hSkills;
+
+    #region Damageable
+    public void Damage(object damage)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Damage(float fDmg)
+    {
+        Hp -= fDmg;
+    }
+    #endregion
+}
+
+public enum ArmorType
+{
+    Light,
+    Medium,
+    Heavy
+}
