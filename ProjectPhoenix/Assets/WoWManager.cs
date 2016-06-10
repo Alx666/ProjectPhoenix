3b73be2966e80e79f084b004129bbc83b5692df4
MarcoMoscati88 <marco.moscati88@gmail.com>
2016-06-10 14:22:26 +0200

    In game UI v1.0
    Score & WoW

---
+++ ProjectPhoenix/Assets/WoWManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class WoWManager : NetworkBehaviour {
+
+	public Text SomeText;
+	private int count = 0;
+	private float time = 1f;
+
+	void Awake()
+	{
+		SomeText = GetComponentInChildren<Text>();
+	}
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+		time -= Time.deltaTime;
+		if ( time <=0f )
+		{
+			SomeText.text += ++count + "\n";
+			time = 0.5f;
+		}
+	}
+}
