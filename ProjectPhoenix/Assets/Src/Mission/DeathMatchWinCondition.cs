3e7212d50585fb8a05644bf83e84a84fb1ae5baf
Frippo <francescofrippo@gmail.com>
2016-06-13 18:02:29 +0200

    Prototype Win Condition
    Molto banale per testing, però pinguini?!11!?

---
+++ ProjectPhoenix/Assets/Src/Mission/DeathMatchWinCondition.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DeathMatchWinCondition : IVictoryCondition
+{
+    public int MaxScore { get; private set; }
+
+    public DeathMatchWinCondition(int iMaxScore)
+    {
+        MaxScore = iMaxScore;
+    }
+
+    public bool Check()
+    {
+        if (GameManager.Instance.GetHighestScore() >= MaxScore)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ ProjectPhoenix/Assets/Src/Mission/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    static public GameManager Instance { get; private set; }
+
+    private IVictoryCondition m_hVictoryCondition;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            throw new System.Exception("Multiple Gamemanager detected in scene!");
+    }
+
+    internal int GetHighestScore()
+    {
+        //ritorna il punteggio piu alto
+        //serve implementazione del punteggio
+        throw new NotImplementedException();
+    }
+
+    void Start()
+    {
+        //ToDo: Rendere victory condition generica
+        m_hVictoryCondition = new DeathMatchWinCondition(20);
+    }
+
+    void Update()
+    {
+        if (m_hVictoryCondition.Check())
+            GameTerminated();
+    }
+
+    void GameTerminated()
+    {
+        Debug.Log("Game finish");
+    }
+}
