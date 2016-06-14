de29d04b8549f61277089ab198db9dc55f7b957a
EnricoCempanari <enrico.cempanari@gmail.com>
2016-06-14 10:51:49 +0200

    Added new environmet assets, VFX, updated some car prefabs

---
+++ ProjectPhoenix/Assets/LightFlicker.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LightFlicker : MonoBehaviour
+{
+
+    public List<Animator> LightAnimators;
+
+    public float RandomTime;
+
+    private float Targettime;
+    private float CurrentTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        CurrentTime = 0.0f;
+        Targettime = Random.Range(2.0f, RandomTime);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        float facaso = 3.0f;
+
+	    if(CurrentTime >= Targettime)
+        {
+            LightAnimators.ForEach(hA => hA.SetTrigger("Flicker"));
+            Targettime = Random.Range(0.0f, RandomTime);
+            CurrentTime = 0.0f;
+            facaso = 4.0f;
+        }
+        else
+        {
+            CurrentTime += Time.deltaTime;
+        }
+	}
+}
