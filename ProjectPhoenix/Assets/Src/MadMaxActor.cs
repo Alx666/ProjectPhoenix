c9994fc74de78e82b3dcd8af9d5d838dcc699bbe
adrN90 <adrian.nom@hotmail.it>
2016-06-17 15:47:45 +0200

    MadMaxActor

---
+++ ProjectPhoenix/Assets/Src/MadMaxActor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MadMaxActor : Actor
+{
+
+    public override void Damage(IDamageSource hSource)
+    {
+        this.Hp -= hSource.GetDamage(this.Armor);
+
+        if (this.Hp == 0f)
+        {
+            this.Die(hSource.Owner);
+        }
+    }
+
+    public override void Die(Actor Killer)
+    {
+        // Gamemanager.NotifyKill(Actor Killer, Actor Killed);
+    }
+}
