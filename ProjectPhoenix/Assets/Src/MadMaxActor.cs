16d51efbcaec0c876a194b1884cb5953719b9c46
MarcoMoscati88 <marco.moscati88@gmail.com>
2016-06-20 16:52:20 +0200

    X Enrico
    Daje

--- ProjectPhoenix/Assets/Src/MadMaxActor.cs
+++ ProjectPhoenix/Assets/Src/MadMaxActor.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.Networking;
 
 public class MadMaxActor : Actor
 {
+	public Slider Slider;
+	public Transform ToLookCamera;
 
+	private float currentHealth;
+
+	void Awake()
+	{
+		currentHealth = Hp;
+		Slider = this.GetComponentInChildren<Slider>();
+	}
+
+	void LateUpdate()
+	{
+		Slider.transform.LookAt( ToLookCamera );
+	}
     public override void Damage(IDamageSource hSource)
     {
         this.Hp -= hSource.GetDamage(this.Armor);
