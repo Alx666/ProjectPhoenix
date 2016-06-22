d23dd33dc69cf1281765c0608510c795480a9602
EnricoCempanari <enrico.cempanari@gmail.com>
2016-06-22 18:59:03 +0200

    Splash art added, vehicle selection updated

---
+++ ProjectPhoenix/Assets/Src/Menu/SplashToMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashToMenu : MonoBehaviour
+{
+
+    public void ToLobby()
+    {
+        SceneManager.LoadScene("DRAIV_Vehicle_Selection");
+    }
+}
