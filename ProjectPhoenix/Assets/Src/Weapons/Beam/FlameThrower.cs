c181a3090820b1983df186c34aacf0cb81286541
EnricoCempanari <enrico.cempanari@gmail.com>
2016-07-23 16:55:13 +0200

    Updated cars and weapons prefabs, SFX/VFX updated. Flamethrower/Laser script audio play/stop added

--- ProjectPhoenix/Assets/Src/Weapons/Beam/FlameThrower.cs
+++ ProjectPhoenix/Assets/Src/Weapons/Beam/FlameThrower.cs
@@ -15,7 +15,8 @@ public class FlameThrower : MonoBehaviour, IBeam
     public float DoT;
 
     private float LightIntensity;
-    
+    private AudioSource FlamesSound;
+
 
     private List<ParticleSystem> m_hParticleSystems;
     void Awake()
@@ -24,6 +25,7 @@ public class FlameThrower : MonoBehaviour, IBeam
         m_hParticleSystems = this.GetComponentsInChildren<ParticleSystem>().ToList();
         FlameLight.enabled = false;
         LightIntensity = FlameLight.intensity;
+        FlamesSound = this.GetComponent<AudioSource>();
     }
 
     public void Enable(Vector3 vPos, Vector3 vDir)
@@ -33,16 +35,19 @@ public class FlameThrower : MonoBehaviour, IBeam
         m_hParticleSystems.ForEach(hP => hP.Play());
         FlameLight.enabled = true;
         FlameLight.intensity = LightIntensity;
+        //TODO: Lerp Volume!!!
+        if (!FlamesSound.isPlaying)
+            FlamesSound.Play();
 
         RaycastHit m_hHitPoint = new RaycastHit();
         Ray vRay = new Ray(transform.position, transform.forward);
 
-        if(Physics.Raycast(vRay, out m_hHitPoint, MaxFireLength))
+        if (Physics.Raycast(vRay, out m_hHitPoint, MaxFireLength))
         {
             IDamageable hHit = m_hHitPoint.collider.GetComponent<IDamageable>();
             FlammableObject m_hFlamObj = m_hHitPoint.collider.GetComponent<FlammableObject>();
-            
-            if(hHit != null)
+
+            if (hHit != null)
                 hHit.Damage(this);
 
             if (m_hFlamObj != null)
@@ -54,6 +59,8 @@ public class FlameThrower : MonoBehaviour, IBeam
     {
         m_hParticleSystems.ForEach(hP => hP.Stop());
         LeanTween.value(FlameLight.gameObject, LightIntensity, 0.0f, 0.5f).setOnUpdate(TweenLightIntensity);
+        //TODO: Lerp Volume!!!
+        FlamesSound.Stop();
     }
 
     private void TweenLightIntensity(float val)
