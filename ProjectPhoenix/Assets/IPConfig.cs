2db5b6be6de560379f1074d9f2d414da3d4fbf4e
Cempix <enrico.cempanari@gmail.com>
2016-10-25 19:13:45 +0200

    Push IPConfig Script + File di prova

---
+++ ProjectPhoenix/Assets/IPConfig.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+using System.Linq;
+
+public class IPConfig : MonoBehaviour
+{
+    InputField inputField;
+    public Text TextChild;
+	void Start ()
+    {
+        inputField = GetComponent<InputField>();
+        string fileText;
+        try
+        {
+            fileText        = File.ReadAllLines(Directory.GetParent((Application.dataPath)) + "/IPConfig.txt")[0].Trim();
+            TextChild.text  = fileText;
+            inputField.text = fileText;
+        }
+        catch
+        {
+            throw new UnityException("IP URL not found");
+        }
+	}
+}
