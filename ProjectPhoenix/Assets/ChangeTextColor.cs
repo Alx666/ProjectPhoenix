61222e8067891de1ac7e7020014efd03a4af7570
Saverio <saverio.dilazzaro@gmail.com>
2017-07-19 21:55:29 +0200

    Updates
    • Added "ChangeTextColor.cs" behaviour on ButtonGUI
    selection/deselection in DRAIV_Vehicle_Selection_Heavy

---
+++ ProjectPhoenix/Assets/ChangeTextColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeTextColor : MonoBehaviour
+{
+    public Color SelectionColor;
+    public float FadeDuration = 0.1f;
+    private Color deselectionColor;
+
+    Text text;
+    Button button;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        button = GetComponentInParent<Button>();
+        deselectionColor = text.color;
+    }
+
+    public void OnSelect()
+    {
+        LeanTween.color(this.gameObject, SelectionColor, FadeDuration).setOnUpdateColor(val => text.color = val);
+    }
+
+    public void OnDeselect()
+    {
+        LeanTween.color(this.gameObject, deselectionColor, FadeDuration).setOnUpdateColor(val => text.color = val);
+    }
+}
